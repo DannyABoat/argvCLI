f36443f18363c7de52378027d38e83986c7edf4c
DannyABoat <dannysum07@yahoo.com>
2022-01-31 14:31:18 +0000

    add multi

--- src/app.js
+++ src/app.js
@@ -7,9 +7,36 @@ const app = (args) => {
             movie.add();
             console.log(movieArr);
             break;
+        case "add multi":
+            const movie1 = new Movie(args[3], args[4]);
+            const movie2 = new Movie(args[5], args[6]);
+            movie1.add();
+            movie2.add();
+            console.log(movieArr);
+            break;
         default:
             console.log("Incorrect command");    
     }
 };
 
 app(process.argv)
+
+
+// const { Player, playerArr } = require("./utils");
+
+// const app = (args) => {
+//     switch (args[2]) {
+//         case "add":
+//             const player = new Player(args[3], args[4]);
+//             player.add();
+//             console.log(playerArr);
+//             break;
+        
+//         default:
+//             console.log("Incorrect Command");
+
+
+//     }
+// };
+
+// app(process.argv)
---
+++ src/utils/myApp.js
@@ -0,0 +1,19 @@
+
+// console.log(process.argv)
+
+// const playerArr = [];
+
+// class Player {
+//     constructor(name, team, position, age = "Not Specified") {
+//         this.name = name
+//         this.team = team
+//         this.position = position
+//         this.age = age
+//     }
+
+//     add() {
+//         playerArr.push(this)
+//     }
+// }
+
+// module.exports = { Player, playerArr };
